7dc385f2a3b1618beabcb375c0a50256b88c6aa0
Ali Sattar <alirsattar@gmail.com>
2018-08-05 19:44:13 -0400

    Added Angular router and created first route from root directory to signup/landing page; moved Angular Material dependencies/imports to separate Material.module file and imported it into main app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }            from '@angular/platform-browser';
+import { NgModule }                 from '@angular/core';
+import { RouterModule, Routes }     from '@angular/router';
+import { HttpModule }               from '@angular/http';
 
-import { AppComponent } from './app.component';
+// v-- IMPORTS FOR ANGULAR MATERIAL UI PLUGINS --v
+import { MaterialModule }           from './material.module'; // <-- TESTING TO SEE IF I CAN ELIMINATE THE
+                                                              //     HUGE LIST OF MATERIAL IMPORTS
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+// ^-- ANIMATIONS MODULE HAS TO COME FIRST
 
-// v-- ALL OF THE LINES BELOW ARE IMPORTING ANGULAR MATERIAL MODULES --v
-import { BrowserAnimationsModule }  from '@angular/platform-browser/animations'; // <-- ANIMATIONS MODULE HAS TO COME FIRST
-import { MatCheckboxModule }        from '@angular/material';
-import { MatButtonModule }          from '@angular/material';
-import { MatInputModule }           from '@angular/material/input';
-import { MatAutocompleteModule }    from '@angular/material/autocomplete';
-import { MatDatepickerModule }      from '@angular/material/datepicker';
-import { MatFormFieldModule }       from '@angular/material/form-field';
-import { MatRadioModule }           from '@angular/material/radio';
-import { MatSelectModule }          from '@angular/material/select';
-import { MatSliderModule }          from '@angular/material/slider';
-import { MatSlideToggleModule }     from '@angular/material/slide-toggle';
-import { MatMenuModule }            from '@angular/material/menu';
-import { MatSidenavModule }         from '@angular/material/sidenav';
-import { MatToolbarModule }         from '@angular/material/toolbar';
-import { MatListModule }            from '@angular/material/list';
-import { MatGridListModule }        from '@angular/material/grid-list';
-import { MatCardModule }            from '@angular/material/card';
-import { MatStepperModule }         from '@angular/material/stepper';
-import { MatTabsModule }            from '@angular/material/tabs';
-import { MatExpansionModule }       from '@angular/material/expansion';
-import { MatButtonToggleModule }    from '@angular/material/button-toggle';
-import { MatChipsModule }           from '@angular/material/chips';
-import { MatIconModule }            from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatProgressBarModule }     from '@angular/material/progress-bar';
-import { MatDialogModule }          from '@angular/material/dialog';
-import { MatTooltipModule }         from '@angular/material/tooltip';
-import { MatSnackBarModule }        from '@angular/material/snack-bar';
-import { MatTableModule }           from '@angular/material/table';
-import { MatSortModule }            from '@angular/material/sort';
-import { MatPaginatorModule }       from '@angular/material/paginator';
+// v-- COMPONENT IMPORTS --v
+import { AppComponent }             from './app.component';
+import { SignupComponent }          from './signup/signup.component';
+
+// v-- SERVICES IMPORTS --v
+import { UserinfoService } from './services/userinfo.service';
+import { FormsModule } from '../../node_modules/@angular/forms';
+
+// v-- ROUTES --v
+const routes: Routes = [
+  {path: '', component: SignupComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [UserinfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
